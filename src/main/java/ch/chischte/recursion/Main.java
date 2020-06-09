867a10fb979e14cc89e67a8118f6e854a81fa606
Michael Wettstein <chischte@gmx.ch>
2020-06-09 07:30:00 +0200

    Initial commit of almost empty intellij project

---
+++ src/main/java/ch/chischte/recursion/Main.java
@@ -0,0 +1,7 @@
+package main.java.ch.chischte.recursion;
+
+public class Main {
+    public static void main (String[] args){
+        System.out.println("works too");
+    }
+}
---
+++ src/main/java/ch/chischte/sorting/Main.java
@@ -0,0 +1,7 @@
+package main.java.ch.chischte.sorting;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("works");
+    }
+}
